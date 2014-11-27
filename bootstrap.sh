87aea2e16889da346295ac0858842c589142b3c1
Mateusz Kwasniewski <mateusz.kwasniewski@schibsted.pl>
2014-11-27 10:04:54 +0100

    install everything that will be needed before ansible runs

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,55 @@
 
 set -e
 
-# Download and install Command Line Tools
-if [[ ! -x /usr/bin/gcc ]]; then
+SRC_DIRECTORY="$HOME/src"
+ANSIBLE_DIRECTORY="$SRC_DIRECTORY/ansible"
+ANSIBLE_CONFIGURATION_DIRECTORY="$HOME/.ansible.d"
+
+# Download and install Command Line Tools with a checking heuristic
+if [[ $(/usr/bin/gcc 2>&1) =~ "no developer tools were found" ]] || [[ ! -x /usr/bin/gcc ]]; then
     echo "Info   | Install   | xcode"
     xcode-select --install
 fi
+
+# Download and install Homebrew
+if [[ ! -x /usr/local/bin/brew ]]; then
+    echo "Info   | Install   | homebrew"
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
+
+echo "Info   | Update   | brew"
+brew update
+
+# Modify the PATH
+export PATH=/usr/local/bin:$PATH
+
+# Download and install zsh
+if [[ ! -x /usr/local/bin/zsh ]]; then
+    echo "Info   | Install   | zsh"
+    brew install zsh
+fi
+
+# Download and install git
+if [[ ! -x /usr/local/bin/git ]]; then
+    echo "Info   | Install   | git"
+    brew install git
+fi
+
+# Download and install python
+if [[ ! -x /usr/local/bin/python ]]; then
+    echo "Info   | Install   | python"
+    brew install python --framework --with-brewed-openssl
+fi
+
+# Download and install Ansible
+if [[ ! -x /usr/local/bin/ansible ]]; then
+    brew install ansible
+fi
+
+# Clone down the Ansible repo
+if [[ ! -d $ANSIBLE_CONFIGURATION_DIRECTORY ]]; then
+    git clone git@github.com:kwasniew/instamac.git $ANSIBLE_CONFIGURATION_DIRECTORY
+    cd $ANSIBLE_CONFIGURATION_DIRECTORY
+    # ansible-playbook main.yml --ask-sudo-pass
+fi
+
